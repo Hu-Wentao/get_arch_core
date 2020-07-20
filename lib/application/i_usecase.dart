331210153bc9724e8842e68d58fa3b473028355c
huwentao <hu.wentao@outlook.com>
2020-07-20 23:16:50 +0800

    add(i_usecase): CompletableUseCase

--- lib/application/i_usecase.dart
+++ lib/application/i_usecase.dart
@@ -10,22 +10,27 @@ import 'package:get_arch_core/get_arch_core.dart';
 ///
 /// 抽象用例
 /// 可以用于 获取最新的数据, 更新/操作数据值,
-/// [Type] 用例(正确的)返回值类型
-/// [Params] 用例的参数类型
-abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
+/// [Out] 用例(正确的)返回值类型
+/// [In] 用例的参数类型
+abstract class UseCase<Out, In> {
+  Future<Either<Failure, Out>> call(In params);
 }
 
 ///
 /// 可以用于获取可观察的数据的用例
-/// [Type] 用例(正确的)返回值类型
-/// [Params] 用例的参数类型
-abstract class ObservableUseCase<Type, Params> {
-  LiveModel<Type> call(Params params);
+/// [Out] 用例(正确的)返回值类型
+/// [In] 用例的参数类型
+abstract class ObservableUseCase<Out, In> {
+  LiveModel<Out> call(In params);
 }
+
+///
+/// 无需返回值的用例
+/// [In] 用例的参数类型
+abstract class CompletableUseCase<In>  extends UseCase<Unit, In>{}
 
 @Deprecated('Please Use [ObservableUseCase]'
     'This feature was deprecated after v2.0.0')
-abstract class StmUseCase<Type, Params> {
-  Stream<Either<Failure, Type>> call(Params params);
+abstract class StmUseCase<Out, In> {
+  Stream<Either<Failure, Out>> call(In params);
 }
