a1a94352963345a49f4880e0c15e8d51b843fa39
huwentao <hu.wentao@outlook.com>
2020-08-04 11:38:57 +0800

    feat(get_arch_core.dart): add export Tuple2, Tuple3;

--- lib/get_arch_core.dart
+++ lib/get_arch_core.dart
@@ -14,6 +14,7 @@ export 'profile/get_arch_application.dart';
 
 export 'package:get_it/get_it.dart';
 export 'package:equatable/equatable.dart';
-export 'package:dartz/dartz.dart' show Either, Unit, left, right, Left, Right;
+export 'package:dartz/dartz.dart'
+    show Either, Unit, left, right, Left, Right, Tuple2, Tuple3;
 export 'package:rxdart/rxdart.dart';
 export 'package:meta/meta.dart';
