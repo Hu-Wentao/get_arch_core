f52dd7a52345b7bcde68856f090ba1e3c42c6faf
huwentao <hu.wentao@outlook.com>
2021-03-04 08:59:32 +0800

    feat(Identity)

--- lib/domain/value_object.dart
+++ lib/domain/value_object.dart
@@ -1,8 +1,15 @@
 import 'package:get_arch_core/gac_base.dart';
 
+///
 /// ValueObject 是 immutable 的
 /// ValueObject 包含值验证逻辑, 一般放置在构造方法内
 abstract class ValueObject extends Equatable {
   @override
   final bool stringify = true;
+}
+
+///
+/// 标识
+abstract class Identity<T> extends ValueObject {
+  T get id;
 }
