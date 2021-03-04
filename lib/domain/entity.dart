8b844268545d1a1a6bf31cf197efd86412e448b7
huwentao <hu.wentao@outlook.com>
2021-03-04 09:00:15 +0800

    feat(Entity): Id must extends Identity

--- lib/domain/entity.dart
+++ lib/domain/entity.dart
@@ -3,11 +3,11 @@
 // Date  : 2020/3/24
 // Time  : 8:31
 
-import 'package:equatable/equatable.dart';
+import 'package:get_arch_core/get_arch_core.dart';
 
 /// 实体类实现本接口
-abstract class IEntity<IdTp> extends Equatable {
-  IdTp get id;
+abstract class IEntity<Id extends Identity> extends Equatable {
+  Id get id;
 
   final bool stringify = true;
 }
