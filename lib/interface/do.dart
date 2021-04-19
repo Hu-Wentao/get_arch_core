2d4dff35a05dc09585fb28a366f90477fc8e761d
huwentao <hu.wentao@outlook.com>
2021-04-19 10:41:53 +0800

    feat: 新增DO使用示例

--- lib/interface/do.dart
+++ lib/interface/do.dart
@@ -4,8 +4,29 @@ import 'dto.dart';
 
 /// DataObject 是对数据表的直接映射
 
-// 数据表与DO类命名
-// foo_bar -> FooBarDO
+/// 数据表与DO类命名
+/// foo_bar -> FooBarDO
+///
+/// example
+/// ```dart
+/// part 'data_object.freezed.dart';
+/// part 'data_object.g.dart';
+///
+/// @freezed
+/// class DeviceDO with _$DeviceDO, IDO {
+///   const DeviceDO._(); // must add this
+///   factory DeviceDO({
+///     int? id,
+///     String? serialNumber,
+///     String? tag,
+///     DeviceType? type,
+///     String? userId,
+///   }) = _DeviceDO;
+///   factory DeviceDO.fromJson(Map<String, dynamic> json) =>
+///       _$DeviceDOFromJson(json);
+/// }
+/// ```
+
 mixin IDO implements IDto {
   String get tableName {
     final len = '$runtimeType'.length;
